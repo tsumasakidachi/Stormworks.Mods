00642510fc97001e2ce23fa35f5fe1b353040b92
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-07-16 00:27:38 +0900

    追加

---
+++ tsu_wedge_corners/src/compile.bat
@@ -0,0 +1,24 @@
+mesh_compiler wedge_corner_1x1x1.dae -o ./
+component_mod_compiler wedge_corner_1x1x1.xml wedge_corner_1x1x1.mesh
+move wedge_corner_1x1x1.bin ..\data\components
+
+mesh_compiler wedge_corner_2x1x1.dae -o ./
+component_mod_compiler wedge_corner_2x1x1.xml wedge_corner_2x1x1.mesh
+move wedge_corner_2x1x1.bin ..\data\components
+
+mesh_compiler wedge_corner_2x2x1.dae -o ./
+component_mod_compiler wedge_corner_2x2x1.xml wedge_corner_2x2x1.mesh
+move wedge_corner_2x2x1.bin ..\data\components
+
+mesh_compiler wedge_corner_2x4x1.dae -o ./
+component_mod_compiler wedge_corner_2x4x1.xml wedge_corner_2x4x1.mesh
+move wedge_corner_2x4x1.bin ..\data\components
+
+mesh_compiler wedge_corner_4x1x1.dae -o ./
+component_mod_compiler wedge_corner_4x1x1.xml wedge_corner_4x1x1.mesh
+move wedge_corner_4x1x1.bin ..\data\components
+
+mesh_compiler wedge_corner_4x4x1.dae -o ./
+component_mod_compiler wedge_corner_4x4x1.xml wedge_corner_4x4x1.mesh
+move wedge_corner_4x4x1.bin ..\data\components
+
