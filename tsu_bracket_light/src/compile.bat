5e0ccb0427673c82675b6e9a93b673e9498b25c6
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-07-20 11:02:05 +0900

    更新

--- tsu_bracket_light/src/compile.bat
+++ tsu_bracket_light/src/compile.bat
@@ -1,4 +1,5 @@
 mesh_compiler bracket_light.dae -o ./
-component_mod_compiler bracket_light.xml bracket_light.mesh
+mesh_compiler bracket_light_glass.dae -o ./
+component_mod_compiler bracket_light.xml bracket_light.mesh bracket_light_glass.mesh
 move bracket_light.bin ..\data\components
 
