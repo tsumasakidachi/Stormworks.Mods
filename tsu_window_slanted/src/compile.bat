f35c0fea426b03975655e663f649e10e4edaafbc
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-07-20 13:51:30 +0900

    更新

--- tsu_window_slanted/src/compile.bat
+++ tsu_window_slanted/src/compile.bat
@@ -10,6 +10,14 @@ mesh_compiler window_slanted_3x1x2.dae -o ./
 component_mod_compiler window_slanted_3x1x2.xml window_slanted_3x1x2.mesh
 move window_slanted_3x1x2.bin ..\data\components
 
+mesh_compiler window_slanted_7x1x2.dae -o ./
+component_mod_compiler window_slanted_7x1x2.xml window_slanted_7x1x2.mesh
+move window_slanted_7x1x2.bin ..\data\components
+
+mesh_compiler window_slanted_9x1x2.dae -o ./
+component_mod_compiler window_slanted_9x1x2.xml window_slanted_9x1x2.mesh
+move window_slanted_9x1x2.bin ..\data\components
+
 mesh_compiler window_slanted_connection_3x1x2.dae -o ./
 component_mod_compiler window_slanted_connection_3x1x2.xml window_slanted_connection_3x1x2.mesh
 move window_slanted_connection_3x1x2.bin ..\data\components
@@ -21,4 +29,16 @@ move window_slanted_connection_4x1x2.bin ..\data\components
 mesh_compiler window_slanted_corner_4x4x2.dae -o ./
 component_mod_compiler window_slanted_corner_4x4x2.xml window_slanted_corner_4x4x2.mesh
 move window_slanted_corner_4x4x2.bin ..\data\components
+
+@REM mesh_compiler window_slanted_3x1x3.dae -o ./
+@REM component_mod_compiler window_slanted_3x1x3.xml window_slanted_3x1x3.mesh
+@REM move window_slanted_3x1x3.bin ..\data\components
+
+@REM mesh_compiler window_slanted_4x1x3.dae -o ./
+@REM component_mod_compiler window_slanted_4x1x3.xml window_slanted_4x1x3.mesh
+@REM move window_slanted_4x1x3.bin ..\data\components
+
+@REM mesh_compiler window_slanted_9x1x3.dae -o ./
+@REM component_mod_compiler window_slanted_9x1x3.xml window_slanted_9x1x3.mesh
+@REM move window_slanted_9x1x3.bin ..\data\components
 
