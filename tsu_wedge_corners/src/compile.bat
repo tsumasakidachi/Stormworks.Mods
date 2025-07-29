2f7dfb20a73c61bbd6eea0ce854e189817330364
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-07-29 21:02:31 +0900

    更新

--- tsu_wedge_corners/src/compile.bat
+++ tsu_wedge_corners/src/compile.bat
@@ -22,3 +22,11 @@ mesh_compiler wedge_corner_4x4x1.dae -o ./
 component_mod_compiler wedge_corner_4x4x1.xml wedge_corner_4x4x1.mesh
 move wedge_corner_4x4x1.bin ..\data\components
 
+mesh_compiler wedge_corner_1x1x2.dae -o ./
+component_mod_compiler wedge_corner_1x1x2.xml wedge_corner_1x1x2.mesh
+move wedge_corner_1x1x2.bin ..\data\components
+
+mesh_compiler wedge_corner_1x1x4.dae -o ./
+component_mod_compiler wedge_corner_1x1x4.xml wedge_corner_1x1x4.mesh
+move wedge_corner_1x1x4.bin ..\data\components
+
