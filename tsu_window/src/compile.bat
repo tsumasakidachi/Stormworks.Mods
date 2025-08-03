07181603d8a50b32734ad7a1bd334ad4b09c93e2
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-08-03 23:59:35 +0900

    更新

--- tsu_window/src/compile.bat
+++ tsu_window/src/compile.bat
@@ -1,6 +1,14 @@
+mesh_compiler window_4x1.dae -o ./
+component_mod_compiler window_4x1.xml window_4x1.mesh
+move window_4x1.bin ..\data\components
+
 mesh_compiler window_4x2.dae -o ./
 component_mod_compiler window_4x2.xml window_4x2.mesh
 move window_4x2.bin ..\data\components
+
+mesh_compiler window_5x1.dae -o ./
+component_mod_compiler window_5x1.xml window_5x1.mesh
+move window_5x1.bin ..\data\components
 
 mesh_compiler window_5x2.dae -o ./
 component_mod_compiler window_5x2.xml window_5x2.mesh
@@ -9,4 +17,12 @@ move window_5x2.bin ..\data\components
 mesh_compiler window_7x2.dae -o ./
 component_mod_compiler window_7x2.xml window_7x2.mesh
 move window_7x2.bin ..\data\components
+
+mesh_compiler window_4x3.dae -o ./
+component_mod_compiler window_4x3.xml window_4x3.mesh
+move window_4x3.bin ..\data\components
+
+mesh_compiler window_5x3.dae -o ./
+component_mod_compiler window_5x3.xml window_5x3.mesh
+move window_5x3.bin ..\data\components
 
