ca43a30d8d5b98737a5f9f7e509439f98d3e9419
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-08-03 17:43:14 +0900

    更新

--- tsu_11/src/compile.bat
+++
@@ -1,4 +0,0 @@
-mesh_compiler window_front.dae -o ./
-component_mod_compiler window_front.xml window_front.mesh
-move window_front.bin ..\data\components
-
---
+++ tsu_window_panoramic/src/compile.bat
@@ -0,0 +1,16 @@
+mesh_compiler window_panoramic_11x1x3.dae -o ./
+component_mod_compiler window_panoramic_11x1x3.xml window_panoramic_11x1x3.mesh
+move window_panoramic_11x1x3.bin ..\data\components
+
+mesh_compiler window_panoramic_slanted_11x1x3.dae -o ./
+component_mod_compiler window_panoramic_slanted_11x1x3.xml window_panoramic_slanted_11x1x3.mesh
+move window_panoramic_slanted_11x1x3.bin ..\data\components
+
+mesh_compiler window_panoramic_11x1x5.dae -o ./
+component_mod_compiler window_panoramic_11x1x5.xml window_panoramic_11x1x5.mesh
+move window_panoramic_11x1x5.bin ..\data\components
+
+mesh_compiler window_panoramic_wedge_11x1x5.dae -o ./
+component_mod_compiler window_panoramic_wedge_11x1x5.xml window_panoramic_wedge_11x1x5.mesh
+move window_panoramic_wedge_11x1x5.bin ..\data\components
+
