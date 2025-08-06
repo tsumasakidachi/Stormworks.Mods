e187ae7395472050fe0dde923a9e052be957b161
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-08-06 23:57:30 +0900

    更新

--- tsu_window/src/compile.bat
+++ tsu_window/src/compile.bat
@@ -6,6 +6,10 @@ mesh_compiler window_4x2.dae -o ./
 component_mod_compiler window_4x2.xml window_4x2.mesh
 move window_4x2.bin ..\data\components
 
+mesh_compiler window_4x3.dae -o ./
+component_mod_compiler window_4x3.xml window_4x3.mesh
+move window_4x3.bin ..\data\components
+
 mesh_compiler window_5x1.dae -o ./
 component_mod_compiler window_5x1.xml window_5x1.mesh
 move window_5x1.bin ..\data\components
@@ -14,15 +18,23 @@ mesh_compiler window_5x2.dae -o ./
 component_mod_compiler window_5x2.xml window_5x2.mesh
 move window_5x2.bin ..\data\components
 
+mesh_compiler window_5x3.dae -o ./
+component_mod_compiler window_5x3.xml window_5x3.mesh
+move window_5x3.bin ..\data\components
+
 mesh_compiler window_7x2.dae -o ./
 component_mod_compiler window_7x2.xml window_7x2.mesh
 move window_7x2.bin ..\data\components
 
-mesh_compiler window_4x3.dae -o ./
-component_mod_compiler window_4x3.xml window_4x3.mesh
-move window_4x3.bin ..\data\components
+mesh_compiler window_7x3.dae -o ./
+component_mod_compiler window_7x3.xml window_7x3.mesh
+move window_7x3.bin ..\data\components
 
-mesh_compiler window_5x3.dae -o ./
-component_mod_compiler window_5x3.xml window_5x3.mesh
-move window_5x3.bin ..\data\components
+mesh_compiler window_9x2.dae -o ./
+component_mod_compiler window_9x2.xml window_9x2.mesh
+move window_9x2.bin ..\data\components
+
+mesh_compiler window_9x3.dae -o ./
+component_mod_compiler window_9x3.xml window_9x3.mesh
+move window_9x3.bin ..\data\components
 
