603fa82577932bc5a51a067f92afb84d0dca609f
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-08-23 14:54:19 +0900

    更新

---
+++ tsu_fender/src/compile.bat
@@ -0,0 +1,12 @@
+mesh_compiler fender_3x2.dae -o ./
+component_mod_compiler fender_3x2.xml fender_3x2.mesh
+move fender_3x2.bin ..\data\components
+
+mesh_compiler fender_1x2.dae -o ./
+component_mod_compiler fender_1x2.xml fender_1x2.mesh
+move fender_1x2.bin ..\data\components
+
+mesh_compiler fender_corner_1x2.dae -o ./
+component_mod_compiler fender_corner_1x2.xml fender_corner_1x2.mesh
+move fender_corner_1x2.bin ..\data\components
+
