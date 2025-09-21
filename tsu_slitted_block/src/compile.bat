a22e5b9423fb3953e1d974e8aa1d9c244b5bd6a1
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-09-21 21:13:52 +0900

    更新

---
+++ tsu_slitted_block/src/compile.bat
@@ -0,0 +1,16 @@
+mesh_compiler slitted_block_edge.dae -o ./
+component_mod_compiler slitted_block_edge.xml slitted_block_edge.mesh
+move slitted_block_edge.bin ..\data\components
+
+mesh_compiler slitted_block_edge_corner_1.dae -o ./
+component_mod_compiler slitted_block_edge_corner_1.xml slitted_block_edge_corner_1.mesh
+move slitted_block_edge_corner_1.bin ..\data\components
+
+mesh_compiler slitted_block_edge_corner_2.dae -o ./
+component_mod_compiler slitted_block_edge_corner_2.xml slitted_block_edge_corner_2.mesh
+move slitted_block_edge_corner_2.bin ..\data\components
+
+mesh_compiler slitted_wedge_edge.dae -o ./
+component_mod_compiler slitted_wedge_edge.xml slitted_wedge_edge.mesh
+move slitted_wedge_edge.bin ..\data\components
+
