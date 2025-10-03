d88ab65f4ae17c47da919addbae4a84659bdb9ba
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-10-03 20:01:06 +0900

    更新

---
+++ tsu_709/src/compile.bat
@@ -0,0 +1,20 @@
+mesh_compiler 709_window_side_1.dae -o ./
+component_mod_compiler 709_window_side_1.xml 709_window_side_1.mesh
+move 709_window_side_1.bin ..\data\components
+
+mesh_compiler 709_window_side_2.dae -o ./
+component_mod_compiler 709_window_side_2.xml 709_window_side_2.mesh
+move 709_window_side_2.bin ..\data\components
+
+mesh_compiler 709_window_side_3.dae -o ./
+component_mod_compiler 709_window_side_3.xml 709_window_side_3.mesh
+move 709_window_side_3.bin ..\data\components
+
+mesh_compiler 709_window_front_center.dae -o ./
+component_mod_compiler 709_window_front_center.xml 709_window_front_center.mesh
+move 709_window_front_center.bin ..\data\components
+
+mesh_compiler 709_window_front_side.dae -o ./
+component_mod_compiler 709_window_front_side.xml 709_window_front_side.mesh
+move 709_window_front_side.bin ..\data\components
+
