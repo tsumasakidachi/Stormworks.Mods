953a55610405be294316051b4c5e189df15b8cff
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-11-26 18:22:20 +0900

    更新

--- tsu_windows/src/compile.bat
+++ tsu_windows/src/compile.bat
@@ -112,9 +112,9 @@ mesh_compiler window_slanted_interface_3x1x3.dae -o ./
 component_mod_compiler window_slanted_interface_3x1x3.xml window_slanted_interface_3x1x3.mesh
 move window_slanted_interface_3x1x3.bin ..\data\components
 
-mesh_compiler window_slanted_interface_3x1x5.dae -o ./
-component_mod_compiler window_slanted_interface_3x1x5.xml window_slanted_interface_3x1x5.mesh
-move window_slanted_interface_3x1x5.bin ..\data\components
+mesh_compiler window_slanted_interface_3x1x6.dae -o ./
+component_mod_compiler window_slanted_interface_3x1x6.xml window_slanted_interface_3x1x6.mesh
+move window_slanted_interface_3x1x6.bin ..\data\components
 
 mesh_compiler window_slanted_interface_4x1x2.dae -o ./
 component_mod_compiler window_slanted_interface_4x1x2.xml window_slanted_interface_4x1x2.mesh
