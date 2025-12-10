c0d0064ea840a83fcb50c9bcfb65c175e9943c63
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-12-10 17:11:05 +0900

    更新

--- tsu_blocks/src/compile.bat
+++ tsu_blocks/src/compile.bat
@@ -14,3 +14,11 @@ mesh_compiler plate_window.dae -o ./
 component_mod_compiler plate_window.xml plate_window.mesh
 move plate_window.bin ..\data\components
 
+mesh_compiler wedge_2_1_2.dae -o ./
+component_mod_compiler wedge_2_1_2.xml wedge_2_1_2.mesh
+move wedge_2_1_2.bin ..\data\components
+
+mesh_compiler wedge_2_2_2.dae -o ./
+component_mod_compiler wedge_2_2_2.xml wedge_2_2_2.mesh
+move wedge_2_2_2.bin ..\data\components
+
