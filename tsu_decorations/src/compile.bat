bee2d7bf151fb4f1a4ad32e6a091c835c6808ebe
tsumasakidachi <tsumasakidachi@outlook.jp>
2025-12-10 20:29:53 +0900

    更新

--- tsu_decorations/src/compile.bat
+++ tsu_decorations/src/compile.bat
@@ -18,7 +18,7 @@ mesh_compiler fender_corner_interface_center_1x3.dae -o ./
 component_mod_compiler fender_corner_interface_center_1x3.xml fender_corner_interface_center_1x3.mesh
 move fender_corner_interface_center_1x3.bin ..\data\components
 
-mesh_compiler fender_corner_inverted_1x3.dae -o ./
-component_mod_compiler fender_corner_inverted_1x3.xml fender_corner_inverted_1x3.mesh
-move fender_corner_inverted_1x3.bin ..\data\components
+mesh_compiler fender_support_1x3.dae -o ./
+component_mod_compiler fender_support_1x3.xml fender_support_1x3.mesh
+move fender_support_1x3.bin ..\data\components
 
