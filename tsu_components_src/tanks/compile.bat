ae370080adf60d051617499710f5af75f42b677e
tsumasakidachi <tsumasakidachi@outlook.jp>
2026-01-11 12:20:50 +0900

    更新

--- tsu_components_src/tanks/compile.bat
+++ tsu_components_src/tanks/compile.bat
@@ -2,3 +2,7 @@ mesh_compiler fluid_tank_3x2x6.dae -o ./
 component_mod_compiler fluid_tank_3x2x6.xml fluid_tank_3x2x6.mesh
 move fluid_tank_3x2x6.bin ../../tsu_components/data/components
 
+mesh_compiler fluid_tank_2x2x2.dae -o ./
+component_mod_compiler fluid_tank_2x2x2.xml fluid_tank_2x2x2.mesh
+move fluid_tank_2x2x2.bin ../../tsu_components/data/components
+
