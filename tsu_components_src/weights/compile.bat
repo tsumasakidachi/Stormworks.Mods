53b26db92670232e9fa030ef4405283afa68cdaa
tsumasakidachi <tsumasakidachi@outlook.jp>
2026-04-25 23:40:09 +0900

    更新

---
+++ tsu_components_src/weights/compile.bat
@@ -0,0 +1,9 @@
+component_mod_compiler weight_20.xml
+move weight_20.bin ../../tsu_components/data/components
+
+component_mod_compiler weight_50.xml
+move weight_50.bin ../../tsu_components/data/components
+
+component_mod_compiler weight_100.xml
+move weight_100.bin ../../tsu_components/data/components
+
