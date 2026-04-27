c20a700942edce5c175614ba7ad2e3b7f1f9be03
tsumasakidachi <tsumasakidachi@outlook.jp>
2026-04-27 15:46:26 +0900

    更新

--- tsu_components_src/fenders/compile.bat
+++ tsu_components_src/fenders/compile.bat
@@ -22,3 +22,7 @@ mesh_compiler fender_support_1x3.dae -o ./
 component_mod_compiler fender_support_1x3.xml fender_support_1x3.mesh
 move fender_support_1x3.bin ../../tsu_components/data/components
 
+mesh_compiler fender_corner_support_1x3.dae -o ./
+component_mod_compiler fender_corner_support_1x3.xml fender_corner_support_1x3.mesh
+move fender_corner_support_1x3.bin ../../tsu_components/data/components
+
