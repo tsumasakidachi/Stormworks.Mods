b96bc5aa3d442fb34fbf52cfcd293e27941300e1
tsumasakidachi <tsumasakidachi@outlook.jp>
2026-05-26 18:43:36 +0900

    更新

--- tsu_components_src/doors/compile.bat
+++ tsu_components_src/doors/compile.bat
@@ -18,8 +18,13 @@ mesh_compiler door_manual_flush_7x3_dynamic.dae -o ./
 component_mod_compiler door_manual_flush_7x3.xml door_manual_flush_7x3.mesh door_manual_flush_7x3_dynamic.mesh
 move door_manual_flush_7x3.bin ../../tsu_components/data/components
 
-mesh_compiler door_manual_window_7x3.dae -o ./
-mesh_compiler door_manual_window_7x3_dynamic.dae -o ./
-component_mod_compiler door_manual_window_7x3.xml door_manual_window_7x3.mesh door_manual_window_7x3_dynamic.mesh
-move door_manual_window_7x3.bin ../../tsu_components/data/components
+mesh_compiler door_manual_flush_inswing_window_7x3.dae -o ./
+mesh_compiler door_manual_flush_inswing_window_7x3_dynamic.dae -o ./
+component_mod_compiler door_manual_flush_inswing_window_7x3.xml door_manual_flush_inswing_window_7x3.mesh door_manual_flush_inswing_window_7x3_dynamic.mesh
+move door_manual_flush_inswing_window_7x3.bin ../../tsu_components/data/components
+
+mesh_compiler door_manual_flush_outswing_window_7x3.dae -o ./
+mesh_compiler door_manual_flush_outswing_window_7x3_dynamic.dae -o ./
+component_mod_compiler door_manual_flush_outswing_window_7x3.xml door_manual_flush_outswing_window_7x3.mesh door_manual_flush_outswing_window_7x3_dynamic.mesh
+move door_manual_flush_outswing_window_7x3.bin ../../tsu_components/data/components
 
