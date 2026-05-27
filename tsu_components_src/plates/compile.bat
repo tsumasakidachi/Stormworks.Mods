db400718130d7a7487ec390df26903e81231c205
tsumasakidachi <tsumasakidachi@outlook.jp>
2026-05-28 00:07:16 +0900

    更新

--- tsu_components_src/plates/compile.bat
+++ tsu_components_src/plates/compile.bat
@@ -2,6 +2,10 @@ mesh_compiler plate_1_2.dae -o ./
 component_mod_compiler plate_1_2.xml plate_1_2.mesh
 move plate_1_2.bin ../../tsu_components/data/components
 
+mesh_compiler plate_1_2_corner_angled_1_1_2.dae -o ./
+component_mod_compiler plate_1_2_corner_angled_1_1_2.xml plate_1_2_corner_angled_1_1_2.mesh
+move plate_1_2_corner_angled_1_1_2.bin ../../tsu_components/data/components
+
 mesh_compiler plate_1_4_1.dae -o ./
 component_mod_compiler plate_1_4_1.xml plate_1_4_1.mesh
 move plate_1_4_1.bin ../../tsu_components/data/components
