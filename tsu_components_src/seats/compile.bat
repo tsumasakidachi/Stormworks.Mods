d6f561346bf09504602f0acc91a0fcf1dcba3a40
tsumasakidachi <tsumasakidachi@outlook.jp>
2026-05-29 18:13:49 +0900

    更新

--- tsu_components_src/seats/compile.bat
+++ tsu_components_src/seats/compile.bat
@@ -2,7 +2,11 @@ mesh_compiler seat_bed_6x2.dae -o ./
 component_mod_compiler seat_bed_6x2.xml seat_bed_6x2.mesh
 move seat_bed_6x2.bin ../../tsu_components/data/components
 
-mesh_compiler seat_driver_tiny.dae -o ./
-component_mod_compiler seat_driver_tiny.xml seat_driver_tiny.mesh
-move seat_driver_tiny.bin ../../tsu_components/data/components
+mesh_compiler seat_car_driver_tiny.dae -o ./
+component_mod_compiler seat_car_driver_tiny.xml seat_car_driver_tiny.mesh
+move seat_car_driver_tiny.bin ../../tsu_components/data/components
+
+mesh_compiler seat_train_driver_tiny.dae -o ./
+component_mod_compiler seat_train_driver_tiny.xml seat_train_driver_tiny.mesh
+move seat_train_driver_tiny.bin ../../tsu_components/data/components
 
