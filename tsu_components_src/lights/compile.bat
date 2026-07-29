908cff75f48d9f3be8c72f61f94d56dac211829a
tsumasakidachi <tsumasakidachi@outlook.jp>
2026-07-29 18:43:56 +0900

    更新

--- tsu_components_src/lights/compile.bat
+++ tsu_components_src/lights/compile.bat
@@ -27,3 +27,24 @@ mesh_compiler nav_light_glass.dae -o ./
 component_mod_compiler nav_light.xml nav_light.mesh nav_light_glass.mesh
 move nav_light.bin ../../tsu_components/data/components
 
+mesh_compiler light_bar.dae -o ./
+mesh_compiler light_bar_glass.dae -o ./
+component_mod_compiler light_bar.xml light_bar.mesh light_bar_glass.mesh
+move light_bar.bin ../../tsu_components/data/components
+component_mod_compiler light_bar_rgb.xml light_bar.mesh light_bar_glass.mesh
+move light_bar_rgb.bin ../../tsu_components/data/components
+
+mesh_compiler light_wedge.dae -o ./
+mesh_compiler light_wedge_glass.dae -o ./
+@REM component_mod_compiler light_wedge.xml light_wedge.mesh light_wedge_glass.mesh
+@REM move light_wedge.bin ../../tsu_components/data/components
+component_mod_compiler light_wedge_rgb.xml light_wedge.mesh light_wedge_glass.mesh
+move light_wedge_rgb.bin ../../tsu_components/data/components
+
+mesh_compiler searchlight_block.dae -o ./
+mesh_compiler searchlight_block_glass.dae -o ./
+@REM component_mod_compiler searchlight_block.xml searchlight_block.mesh searchlight_block_glass.mesh
+@REM move searchlight_block.bin ../../tsu_components/data/components
+component_mod_compiler searchlight_block.xml searchlight_block.mesh searchlight_block_glass.mesh
+move searchlight_block.bin ../../tsu_components/data/components
+
