33f0985b353cd0f4bbd19f4511637f4ec83ddf9b
tsumasakidachi <tsumasakidachi@outlook.jp>
2026-08-08 20:30:08 +0900

    更新

--- tsu_components_src/modular_engines/compile.bat
+++ tsu_components_src/modular_engines/compile.bat
@@ -10,6 +10,10 @@ mesh_compiler modular_engine_crankshaft_1x1x1.dae -o ./
 component_mod_compiler modular_engine_crankshaft_1x1x1.xml modular_engine_crankshaft_1x1x1.mesh
 move modular_engine_crankshaft_1x1x1.bin ../../tsu_components/data/components
 
+mesh_compiler modular_engine_crankshaft_3x3x3.dae -o ./
+component_mod_compiler modular_engine_crankshaft_3x3x3.xml modular_engine_crankshaft_3x3x3.mesh
+move modular_engine_crankshaft_3x3x3.bin ../../tsu_components/data/components
+
 mesh_compiler modular_engine_cylinder_1x1x2.dae -o ./
 component_mod_compiler modular_engine_cylinder_1x1x2.xml modular_engine_cylinder_1x1x2.mesh
 move modular_engine_cylinder_1x1x2.bin ../../tsu_components/data/components
@@ -17,6 +21,10 @@ move modular_engine_cylinder_1x1x2.bin ../../tsu_components/data/components
 mesh_compiler modular_engine_cylinder_1x3x1.dae -o ./
 component_mod_compiler modular_engine_cylinder_1x3x1.xml modular_engine_cylinder_1x3x1.mesh
 move modular_engine_cylinder_1x3x1.bin ../../tsu_components/data/components
+
+mesh_compiler modular_engine_cylinder_3x3x3.dae -o ./
+component_mod_compiler modular_engine_cylinder_3x3x3.xml modular_engine_cylinder_3x3x3.mesh
+move modular_engine_cylinder_3x3x3.bin ../../tsu_components/data/components
 
 mesh_compiler modular_engine_exhaust_manifold_housing_corner.dae -o ./
 component_mod_compiler modular_engine_exhaust_manifold_housing_corner.xml modular_engine_exhaust_manifold_housing_corner.mesh
@@ -49,6 +57,10 @@ move modular_engine_clutch_housing_1x1.bin ../../tsu_components/data/components
 mesh_compiler modular_engine_power_manifold_housing_1x1.dae -o ./
 component_mod_compiler modular_engine_power_manifold_housing_1x1.xml modular_engine_power_manifold_housing_1x1.mesh
 move modular_engine_power_manifold_housing_1x1.bin ../../tsu_components/data/components
+
+mesh_compiler modular_engine_power_manifold_housing_3x3.dae -o ./
+component_mod_compiler modular_engine_power_manifold_housing_3x3.xml modular_engine_power_manifold_housing_3x3.mesh
+move modular_engine_power_manifold_housing_3x3.bin ../../tsu_components/data/components
 
 mesh_compiler modular_engine_fluid_pump_housing.dae -o ./
 component_mod_compiler modular_engine_fluid_pump_housing.xml modular_engine_fluid_pump_housing.mesh
---
+++ tsu_components_src/pumps/compile.bat
@@ -0,0 +1,4 @@
+mesh_compiler turbocharger_paintable_small.dae -o ./
+component_mod_compiler turbocharger_paintable_small.xml turbocharger_paintable_small.mesh
+move turbocharger_paintable_small.bin ../../tsu_components/data/components
+
