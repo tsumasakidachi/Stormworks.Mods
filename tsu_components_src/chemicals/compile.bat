2d1277384f07dc14751ea030e1b47fee3e98aa84
tsumasakidachi <tsumasakidachi@outlook.jp>
2026-08-17 15:05:55 +0900

    更新

---
+++ tsu_components_src/chemicals/compile.bat
@@ -0,0 +1,4 @@
+mesh_compiler catalytic_converter_paintable_1x1.dae -o ./
+component_mod_compiler catalytic_converter_paintable_1x1.xml catalytic_converter_paintable_1x1.mesh
+move catalytic_converter_paintable_1x1.bin ../../tsu_components/data/components
+
