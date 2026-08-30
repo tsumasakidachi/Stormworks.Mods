9df9377dc013ba890749dcc58693ae86bd5915c6
tsumasakidachi <tsumasakidachi@outlook.jp>
2026-08-30 19:18:13 +0900

    更新

--- tsu_ports_src/compile.bat
+++ tsu_ports_src/compile.bat
@@ -1,2 +1,6 @@
 mesh_compiler dock_south_harbour.dae -o ../tsu_ports/meshes/tsu_port
 mesh_compiler dock_south_harbour_phys.dae -o ../tsu_ports/meshes/tsu_port -m physics_mesh
+
+mesh_compiler dock_military_base.dae -o ../tsu_ports/meshes/tsu_port
+mesh_compiler dock_military_base_phys.dae -o ../tsu_ports/meshes/tsu_port -m physics_mesh
+
